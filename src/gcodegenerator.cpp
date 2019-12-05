fde23e33776fd5986a0fa1833b4dd5870a576b1f
janavoes <janavoes@gmail.com>
2019-12-05 16:19:59 +0100

    starting sequence updated

--- src/gcodegenerator.cpp
+++ src/gcodegenerator.cpp
@@ -41,13 +41,13 @@ string GCodeGenerator::getGcodeSlice(const std::vector<QPolygon> slice, double z
   //for each polygon generate movement
   for(auto i: slice){
     //nozzle down
-    out += "G0 Z" + std::to_string(-2) + " ; lift up between polygons\n";
+    //out += "G0 Z" + std::to_string(-2) + " ; lift up between polygons\n";
 
     //move to starting point of the polygon
     QPoint startPolygon = i.front();
     out += getVectorMovementXY(startPolygon.x()/INT_SCALE, startPolygon.y()/INT_SCALE, m_extrusion);
     QPoint lastPoint = i.front();
-
+    out += "G0 Z" + std::to_string(-2) + " ; down\n";
     //connect each vertex of the polygon
     for(auto j: i){
       //layer thickness = 0.2 ==> 0.36 mm wide
@@ -98,20 +98,29 @@ QPoint GCodeGenerator::getLastCollisionX(QPolygon polygon, QPoint start){
 }
 
 //heat the bed/nozzle + extrude a bit
-string GCodeGenerator::getStartSequence(int bedTemperature, int nozzleTemperature){
-  string out = "G21 ;metricvalues\nG90 ; absolute positioning\nM82 ; extruder to absolute mode \nM107 ; start with fan off \n";
-
-  /*
-  G1 X0 Y{machine_depth} ;Present print
-  M106 S0 ;Turn-off fan
-  M104 S0 ;Turn-off hotend
-  M140 S0 ;Turn-off bed
-  */
+string GCodeGenerator::getStartSequence(const int bedTemperature,const int nozzleTemperature){
+  string out = "";
+  //"G21 ;metricvalues\nG90 ; absolute positioning\nM82 ; extruder to absolute mode \nM107 ; start with fan off \n";
 
   out += "M104 S" + std::to_string(nozzleTemperature) + " ; set temp nozzle but do not wait\nM190 S" + std::to_string(bedTemperature) + " ; set bed temp and wait\n" + "M109 S" + std::to_string(nozzleTemperature) +"; wait until temps are reached\n";
-
+  out += "M201 X500.00 Y500.00 Z100.00 E5000.00 ;Setup machine max acceleration\n";
+  out += "M203 X500.00 Y500.00 Z10.00 E50.00 ;Setup machine max feedrate\n";
+  out += "M204 P500.00 R1000.00 T500.00 ;Setup Print/Retract/Travel acceleration\n";
+  out += "M205 X8.00 Y8.00 Z0.40 E5.00 ;Setup Jerk\n";
+  out += "M220 S100 ;Reset Feedrate\n";
+  out += "M221 S100 ;Reset Flowrate\n";
+  out += "G28 ;Home\n";
+  out += "G92 E0 ;Reset Extruder\n";
+  out += "G1 Z2.0 F3000 ;Move Z Axis up\n";
+  out += "G1 X10.1 Y20 Z0.28 F5000.0 ;Move to start position\n";
+  out += "G1 X10.1 Y200.0 Z0.28 F1500.0 E15 ;Draw the first line\n";
+  out += "G1 X10.4 Y200.0 Z0.28 F5000.0 ;Move to side a little\n";
+  out += "G1 X10.4 Y20 Z0.28 F1500.0 E30 ;Draw the second line\n";
+  out += "G92 E0 ;Reset Extruder\n";
+  out += "G1 Z2.0 F3000 ;Move Z Axis up\n";
   //out += "G28 X0 Y0 ; X,Y to min endstops \nG28 Z0 ; z to min endstops \nG1 Z15.0 F9000 ; move platform down 15 mm \nG92 E0 ; zero extruded length \nG1 F200 E3 ; extrude 3mm \nG92 E0 ; zero extrude length \nG1 F9000 ; add text printing on lcd \nM 117 Printing\nG1 F1000\n";
-  out +=   "G91 ;Relative positionning\nG1 E-2 F2700 ;Retract a bit\nG1 E-2 Z0.2 F2400 ;Retract and raise Z \nG1 X5 Y5 F3000 ;Wipe out \nG1 Z10 ;Raise Z more\nG90 ;Absolute positionning";
+
+  //end sequence out +=   "G91 ;Relative positioning\nG1 E-2 F2700 ;Retract a bit\nG1 E-2 Z0.2 F2400 ;Retract and raise Z \nG1 X5 Y5 F3000 ;Wipe out \nG1 Z10 ;Raise Z more\nG90 ;Absolute positionning";
 
   return out;
 }
--- src/gcodegenerator.h
+++ src/gcodegenerator.h
@@ -18,16 +18,16 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-const long INT_SCALE = 1000000;
+const long INT_SCALE = 10000;
 
 class GCodeGenerator{
 public:
   GCodeGenerator();
   void generateGcode(const std::vector<std::vector<QPolygon>> slices, const string filename);
-  void setLayerHeight(double height);
+  void setLayerHeight(const double height);
 
 private:
-  string getStartSequence(int bedTemperature, int nozzleTemperature);
+  string getStartSequence(const int bedTemperature,const int nozzleTemperature);
   string getEndSequence();
   string getGcodeSlice(std::vector<QPolygon> slice, double z);
   string getVectorMovementXY(double X, double Y, double extrusion);
