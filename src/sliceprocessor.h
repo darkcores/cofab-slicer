b0ede3456d47f332bde27e1eb6972364f51d332a
Jorrit Gerets <jorrit@darkcores.net>
2019-12-19 17:41:35 +0100

    GUI infill spacing, wall, roofs, floors.

--- src/sliceprocessor.h
+++ src/sliceprocessor.h
@@ -24,11 +24,15 @@ class SliceProcessor {
     std::vector<std::vector<QPolygon>>
     process(const std::vector<std::vector<QPolygon>> &paths);
 
+	void setWalls(unsigned int w) { num_walls = w; }
+	void setRoofsFloors(unsigned int rf) { num_rf = rf; }
+	void setInfillSpacing(int spacing) { infill_offset = spacing; }
+
   private:
     const QRect bounds;
     unsigned int num_walls = 2; // Min 1
     unsigned int num_rf = 2;    // Num roofs / floors
-    const int infill_offset = 15;
+    int infill_offset = 15;
 	const int wall_width = 0.3771 * 1000; // extrusion width - layerheight * (1-pi/4)
     const long nozzle_offset = -wall_width;
     std::vector<ClipperLib::Paths> clipped_slices;
--- src/sliceroptions.h
+++ src/sliceroptions.h
@@ -2,6 +2,8 @@
 #define SLICEROPTIONS_H
 
 #include <QDockWidget>
+#include <QSpinBox>
+#include <QDoubleSpinBox>
 
 class QCheckBox;
 class QLineEdit;
@@ -11,10 +13,16 @@ class SlicerOptions : public QDockWidget {
   public:
     explicit SlicerOptions(QWidget *parent = nullptr);
 
+	int infill() { return infillBox->value(); }
+	int walls() { return wallsBox->value(); }
+	int floorroof() { return floorroofBox->value(); }
+
   private:
     QWidget *objectSettings();
     QWidget *slicerSettings();
     QWidget *printerSettings();
+
+	QSpinBox *infillBox, *wallsBox, *floorroofBox;
 
   signals:
 
